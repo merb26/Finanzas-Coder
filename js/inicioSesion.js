6c207df490adb7ce522f0a02f0761be0357af34c
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2022-05-26 04:25:41 -0500

    1

--- js/inicioSesion.js
+++ js/inicioSesion.js
@@ -28,6 +28,7 @@ btnIniciar.onclick = () => {
             if ((txtUsuario === nombre || txtUsuario === email) && pass === passUsuario) {
                 isFind = true
                 sessionStorage.setItem('usuario', nombre)
+                alert("Dinero: " + dinero)
                 sessionStorage.setItem('dineroDisponible', dinero)
                 sessionStorage.setItem('nuevoUsuario', false)
                 sessionStorage.setItem('muestraMensaje', true)
